9488df3837cad210914909d877f931ed95817149
shakina <shakinaabuthahir@gmail.com>
2025-06-30 15:07:31 +0530

    name

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import React, { useState } from 'react';
 
 function App() {
   const[dark,setDark]=useState(false);
+  const [name,setName]=useState("");
   return (
     <div className={dark?"container dark":"container"}>
       <h1>Simple Counter App</h1>
+      <input type="text" placeholder="enter your name" value={name}
+      onChange={(e)=>setName(e.target.value)}
+      ></input>
+      <p>Hello,{name?name:"User"}</p>
       <Counter></Counter>
      <button className="toggle-btn" onClick={() => setDark(!dark)}>
         {dark ?"☀️":"🌙"}
